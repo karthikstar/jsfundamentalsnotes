7afe66106ffc3db49573f6c68fdffa59feb2856c
elkarthi2000@gmail.com <elkarthi2000@gmail.com>
2020-09-29 23:26:23 +0800

    added new segments - removing, replacing and adding new elements

--- 03_traversingDom/js_sandbox/app.js
+++ 03_traversingDom/js_sandbox/app.js
@@ -63,3 +63,40 @@ val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
 
     
 console.log(val);
+
+// Creating Elements
+
+// we can construct DOM elements from scratch using vanilla js
+const li = document.createElement('li');
+
+// Add Class
+li.className = 'collection-item';
+
+// Add an id
+li.id = 'new-item';
+
+// Add attribute
+li.setAttribute('title','New Item');
+
+// Create text node and append
+
+li.appendChild(document.createTextNode('Hello World'));
+
+// Create new link element
+const link = document.createElement('a');
+// Add classes 
+
+link.className = 'delete-item secondary-content';
+
+// Add icon html 
+link.innerHTML = '<i class = "fa fa-remove"></i>';
+
+// append link into li 
+li.appendChild(link);
+
+// Append li as child to ul 
+
+document.querySelector('ul.collection').appendChild(li);
+
+console.log(li);
+
