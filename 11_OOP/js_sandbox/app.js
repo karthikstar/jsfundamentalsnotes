af43d5fe419981dfeaa2b0173d4d9d39a494ec08
elkarthi2000@gmail.com <elkarthi2000@gmail.com>
2020-11-05 23:56:53 +0800

    added es6 classes, inheritance

--- 11_OOP/js_sandbox/app.js
+++ 11_OOP/js_sandbox/app.js
@@ -176,60 +176,185 @@
 
 // -------------------------------
 
-// 4. Prototypal inheritance 
+// // 4. Prototypal inheritance 
 
-// whjat if we want one object inherit from another ? create a obj and a customer obj which will inherit its prototype
+// // whjat if we want one object inherit from another ? create a obj and a customer obj which will inherit its prototype
 
-// Person Cosntructor 
+// // Person Cosntructor 
 
-function Person(firstName, lastName){
-  this.firstName = firstName;
-  this.lastName = lastName;
-}
+// function Person(firstName, lastName){
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+// }
 
-// Greeting 
-Person.prototype.greeting = function(){
-  return `Hello there ${this.firstName} ${this.lastName}`;
-}
+// // Greeting 
+// Person.prototype.greeting = function(){
+//   return `Hello there ${this.firstName} ${this.lastName}`;
+// }
 
-const person1 = new Person('John','Doe');
+// const person1 = new Person('John','Doe');
 
-console.log(person1.greeting());
+// console.log(person1.greeting());
 
-// create a constructor for a customer
+// // create a constructor for a customer
 
-// Customer Constructor
-function Customer(firstName, lastName, phone, membership){
-  // call Person constructor
-  // call is a function that allows us to call another function from another constructor 
+// // Customer Constructor
+// function Customer(firstName, lastName, phone, membership){
+//   // call Person constructor
+//   // call is a function that allows us to call another function from another constructor 
 
-  Person.call(this,firstName,lastName); // add whatever paroperties from ther constructor we are inheriting
+//   Person.call(this,firstName,lastName); // add whatever paroperties from ther constructor we are inheriting
   
-  // extra properties that we have for the customer function, we add it here like how we nornmally do
-  this.phone = phone;
-  this.membership = membership;
+//   // extra properties that we have for the customer function, we add it here like how we nornmally do
+//   this.phone = phone;
+//   this.membership = membership;
   
+// }
+
+// // Inherit the Person Prototype methods 
+// Customer.prototype = Object.create(Person.prototype);
+
+
+// // Make customer.prototype return Customer()
+
+// Customer.prototype.constructor = Customer; 
+
+
+
+// // Create customer
+// const customer1 = new Customer('tom','smith',12232,'standard');
+// console.log(customer1)
+
+// // Customer greeting - overwrite the greeting in person.prototype
+
+// Customer.prototype.greeting = function(){
+//   return `Hello there ${this.firstName} ${this.lastName}. Welcome to our company`;
+
+// }
+
+// console.log(customer1.greeting()); 
+
+
+// -----------------------
+
+// // 5. Using Object.create - another way to create objs 
+// // method 1
+// const personPrototypes = {
+//     greeting: function(){
+//         return `hello there ${this.firstName} ${this.lastName}`;
+//     },
+//     getsMarried: function(newLastName){
+//         this.lastName = newLastName;
+//     }
+
+// }
+
+// const mary = Object.create(personPrototypes);
+// mary.firstName = 'Mary';
+// mary.lastName = 'Williams';
+
+// mary.age = 30;
+
+// mary.getsMarried('Thompson');
+
+// console.log(mary.greeting());
+
+// // method 2
+// const brad = Object.create(personPrototypes, {
+//     firstName: {value:'Brad'},
+//     lastName: {value:'Cook'},
+//     age:{value:20}
+// });
+
+// console.log(brad);
+// console.log(brad.greeting());
+// //  this is an alternative way to create objects using object.create
+
+
+//--------------------------
+// //  now that we have seen how OOP works in ES5 syntax, lets look at Es6 classes! 
+
+// // 6. ES6 Classes 
+
+// class Person {
+//     constructor(firstName,lastName, dob){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.birthday = new Date(dob);
+//     }
+
+//     greeting(){
+//         return `hello there ${this.firstName} ${this.lastName}`;
+//     } // any method that we add inside the class is gonna be added to the prototype(advantage)
+
+//     calculateAge(){
+//         const diff = Date.now() - this.birthday.getTime();
+//         const ageDate = new Date(diff);
+//         return Math.abs(ageDate.getUTCFullYear() - 1970)
+//     }
+
+//     getsMarried(newLastName){
+//         this.lastName = newLastName;
+//     }
+
+//     static addNumbers(x,y){
+//         return x + y;
+//     } // static method - its not part of the Mary instance
+// }
+
+// // creating an obj from a class
+// const Mary = new Person('Mary','Williams','11-13-1980');
+// Mary.getsMarried('Thompson');
+// console.log(Mary);
+
+
+// // static methods - method that we can use without instantiating an object - eg create a method to add 2 numbers tgt 
+
+// // to use the static method, use the actual class name.
+// console.log(Person.addNumbers(2,2))
+
+// // classes are called syntartic sugar as they only change the way we write them but not under the hood / engien
+
+
+// ----------------------------
+// 7. Sub classes - inheritance in es6 classes 
+
+class Person {
+    constructor(firstName,lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    greeting(){
+        return `Hello there ${this.firstName} ${this.lastName}`;
+    }
 }
 
-// Inherit the Person Prototype methods 
-Customer.prototype = Object.create(Person.prototype);
+const person1 = new Person('bob','cook')
+Person.prototype.sample = function(){
+    return 2
+}
+console.log(person1)
+console.log(Person.prototype.sample())
 
+// creating a sub class of Person -  Customer
+class Customer extends Person {
+    constructor(firstName,lastName,phone,membership){
+        // when we instantiate Customer, since its extending a person, we want to call the Person constructor and we do that by using super
+        // super calls the parent class's constructor.
+        super(firstName,lastName); // we have to pass in here the two params
 
-// Make customer.prototype return Customer()
-
-Customer.prototype.constructor = Customer; 
-
-
-
-// Create customer
-const customer1 = new Customer('tom','smith',12232,'standard');
-console.log(customer1)
-
-// Customer greeting - overwrite the greeting in person.prototype
-
-Customer.prototype.greeting = function(){
-  return `Hello there ${this.firstName} ${this.lastName}. Welcome to our company`;
-
+        // anything thats not a property of Person and just in the customer class, we write below. 
+        this.phone = phone;
+        this.membership = membership;
+    }
+    static getMembershipCost(){
+        return 500;
+    }
 }
 
-console.log(customer1.greeting()); 
+const john = new Customer('John','Doe','2323232','Standard');
+// since we extended Person, we can use any methods in it
+
+console.log(john.greeting())
+
+console.log(Customer.getMembershipCost());
