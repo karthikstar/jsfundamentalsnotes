5fd7441c62fc1166eacdf7a29a0fa61c1738efcb
elkarthi2000@gmail.com <elkarthi2000@gmail.com>
2020-11-08 17:16:08 +0800

    completed es5, es6 versions of proj

--- 12_OOP-Booklist Project/app.js
+++ 12_OOP-Booklist Project/app.js
@@ -29,8 +29,40 @@ UI.prototype.addBookToList = function(book){
     list.appendChild(row);
 
 }
+// Show Alert -adding showalert method to UI prototype
+UI.prototype.showAlert = function(message, className){
+    // Create div
+    const div = document.createElement('div');
+    // Add Classes
+    div.className = `alert ${className}`;
+    // Add Text
+    div.appendChild(document.createTextNode(message));
 
-// Clear fields
+    // Get parent
+    const container = document.querySelector('.container');
+    
+    const form = document.querySelector('#book-form');
+
+    // Insert alert into DOM ,above the form
+    container.insertBefore(div,form); // takes 2 params - one is the element which we are inserting, 2nd is before which element do u want to insert in .
+    
+    // Timeout after 3 sec and remove the alert element.
+    setTimeout(function(){
+        document.querySelector('.alert').remove(); 
+    }, 3000);
+
+
+}
+// Delete Book - adding deleteBook method to ui prototype
+UI.prototype.deleteBook = function(target){
+    if (target.className === 'delete'){
+        // target is an a tag, to remove whole row, we need remove its parent's parent element.
+        target.parentElement.parentElement.remove();
+
+    }
+}
+
+// Clear fields - addign clearfields method to ui prototype
 
 UI.prototype.clearFields = function(){
     document.getElementById('title').value = '';
@@ -38,7 +70,8 @@ UI.prototype.clearFields = function(){
     document.getElementById('isbn').value = '';
 }
 
-// Event Listeners
+// Event Listener for add book 
+
 document.getElementById('book-form').addEventListener('submit',function(e){
     // Get form values 
     const title = document.getElementById('title').value,
@@ -54,11 +87,35 @@ document.getElementById('book-form').addEventListener('submit',function(e){
     // Instantiate UI object
     const ui = new UI();
 
-    // Add book to List
-    ui.addBookToList(book);
+    // Validate
+    if (title === ''|| author === ''|| isbn === ''){
+        // Error alert
+        ui.showAlert('Please fill in all fields', 'error');
 
-    // Clear fields
-    ui.clearFields();
+    } else {
+        // Add book to List
+        ui.addBookToList(book);
 
+        // Show Success Alert
+        ui.showAlert('Book added!','success');
+
+        // Clear fields
+        ui.clearFields();
+
+    }
+
+
+    e.preventDefault();
+})
+
+// Event Listener for Delete button
+document.getElementById('book-list').addEventListener('click',function(e){
+    // Instantiate UI 
+    const ui = new UI();
+    // Delete Book
+    ui.deleteBook(e.target);
+
+    // Show message
+    ui.showAlert('Book Removed!','success');
     e.preventDefault();
 })
---
+++ 12_OOP-Booklist Project/appes6.js
@@ -0,0 +1,114 @@
+class Book {
+    constructor(title,author,isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+class UI {
+    addBookToList(book){
+        const list = document.getElementById('book-list');
+
+        // create tr elemenet
+        const row = document.createElement('tr');
+    
+        // Inert Cols
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href = "#" class = "delete">X<a></td>
+        `;
+        list.appendChild(row);
+
+    }
+    showAlert(message, className){
+        // Create div
+        const div = document.createElement('div');
+        // Add Classes
+        div.className = `alert ${className}`;
+        // Add Text
+        div.appendChild(document.createTextNode(message));
+
+        // Get parent
+        const container = document.querySelector('.container');
+        
+        const form = document.querySelector('#book-form');
+
+        // Insert alert into DOM ,above the form
+        container.insertBefore(div,form); // takes 2 params - one is the element which we are inserting, 2nd is before which element do u want to insert in .
+        
+        // Timeout after 3 sec and remove the alert element.
+        setTimeout(function(){
+            document.querySelector('.alert').remove(); 
+        }, 3000);
+
+
+    }
+    deleteBook(target){
+        if (target.className === 'delete'){
+            // target is an a tag, to remove whole row, we need remove its parent's parent element.
+            target.parentElement.parentElement.remove();
+    
+        }
+    }
+    clearFields(){
+        document.getElementById('title').value = '';
+        document.getElementById('author').value = '';
+        document.getElementById('isbn').value = '';
+    }
+
+}
+
+
+
+// Event Listener for add book 
+
+document.getElementById('book-form').addEventListener('submit',function(e){
+    // Get form values 
+    const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value
+    
+    // console.log(title,author,isbn);
+
+    // Instantiate book
+    const book = new Book(title,author,isbn);
+    // console.log(book);
+    
+    // Instantiate UI object
+    const ui = new UI();
+
+    // Validate
+    if (title === ''|| author === ''|| isbn === ''){
+        // Error alert
+        ui.showAlert('Please fill in all fields', 'error');
+
+    } else {
+        // Add book to List
+        ui.addBookToList(book);
+
+        // Show Success Alert
+        ui.showAlert('Book added!','success');
+
+        // Clear fields
+        ui.clearFields();
+
+    }
+
+
+    e.preventDefault();
+})
+
+// Event Listener for Delete button
+document.getElementById('book-list').addEventListener('click',function(e){
+    // Instantiate UI 
+    const ui = new UI();
+    // Delete Book
+    ui.deleteBook(e.target);
+
+    // Show message
+    ui.showAlert('Book Removed!','success');
+    e.preventDefault();
+})
