dcf70b8ab134da6db729ff2fb19fa1e959ad58cb
elkarthi2000@gmail.com <elkarthi2000@gmail.com>
2020-12-01 00:27:03 +0800

    added mediator pattern

--- 17_Patterns/05_MediatorPattern/app.js
+++ 17_Patterns/05_MediatorPattern/app.js
@@ -13,23 +13,57 @@ const User = function(name){
 
 User.prototype = {
     send:function(message,to){
-        this.chatroom.send(message,this,to); // this pertains to the user
+        // takes in 2 params : msg, and the to user.
+        this.chatroom.send(message,this,to); // 'this' pertains to the user. (msg, user, to user)
 
     },
     receive:function(message,from){
+        // msg , and from user
         console.log(`${from.name} to ${this.name}: ${message}`);
     }
 }
+// idea is for user to send single msg to another user / to send a msg to everyone in the rm
+// chatrm is mediator, users are the callings of the chatrm
 
 const Chatroom = function(){
     let users = {}; // list of users
-
+    // callings have the register w the mediator
     return {
         register:function(user){
             users[user.name] = user;
             user.chatroom = this;
-            
+ 
+        },
+        send: function(message,from,to){
+            if(to){
+                // single user message
+                to.receive(message,from)
+            } else {   
+                // Mass message
+                for(key in users){
+                    if(users[key] !== from){ // make sure its not the user whos sending
+                        // we are making sure the from user doesnt receive the msg that he sent to others.
+                        users[key].receive(message,from);
+                    }
+                }
+            }
         }
     }
 
 }
+
+const brad = new User('Brad');
+const jeff = new User('Jeff');
+const sara = new User('Sara');
+
+// create the mediator
+const chatroom = new Chatroom();
+
+// register the users
+chatroom.register(brad);
+chatroom.register(jeff);
+chatroom.register(sara);
+
+brad.send('hello jeff',jeff);
+sara.send('hi brad', brad);
+jeff.send('hello everyone!');
